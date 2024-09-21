50112f853435403c2c4c243f2c690fc6bc69ede7
prayag <prayagahire365@gmail.com>
2024-09-21 16:32:48 +0530

    add github Oauth and created .env file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,22 @@
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
+console.log(process.env.GITHUB_ID);
+console.log(process.env.GITHUB_SECRET);
+
+// export function GET(){
+//     return NextResponse.json({
+//         message:"hii this is next-auth"
+//     })
+// }
+const handler = NextAuth({
+    providers: [
+        GitHubProvider({
+          clientId: process.env.GITHUB_ID ?? '',
+          clientSecret: process.env.GITHUB_SECRET ?? ''
+        })
+      ]
+})
+export {handler as GET , handler as POST}
+// export const GET = handler;
+// export const POST = handler;
