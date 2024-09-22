9e9cb996bd82f8acd4f91063ee47db92a69e2fd9
prayag <prayagahire365@gmail.com>
2024-09-22 15:25:47 +0530

    add upvote and downvote in stream and created api routes

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,7 +1,9 @@
-import { error } from "console";
+import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import {z} from "zod";
 
+
+const YT_REGEX = new RegExp("^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(?:-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|live\/|v\/)?)([\w\-]+)(\S+)?$")
 const StreamSchema = z.object({
     createrId: z.string(),
     url: z.string()
@@ -11,6 +13,31 @@ const StreamSchema = z.object({
 export async function POST(req:NextRequest){
     try{
         const data = StreamSchema.parse(await req.json());
+        const isYt = YT_REGEX.test(data.url);
+
+        if(!isYt){
+            NextResponse.json({
+                message:"your have inputed wrong URL"
+            },{
+                status:411
+            })
+        }
+
+        const extrectedId = data.url.split("?v=")[1]; 
+        const user = await prismaClient.stream.create({
+            data:{
+                userId: data.createrId,
+                url: data.url,
+                extrectedId,
+                type: "YouTube"
+
+            }
+        });
+        return NextResponse.json({
+            message:"added Stream",
+            id: user.id 
+        })
+
     }catch(e){
         NextResponse.json({
             message:"Error while adding a stream"
---
+++ app/api/streams/upvote/route.ts
@@ -0,0 +1,65 @@
+import { prismaClient } from "@/app/lib/db";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const UpvoteSchema = z.object({
+    streamid: z.string(),
+    userId: z.string(),
+
+}) 
+
+export async function POST(req:NextRequest){
+
+
+    // here we first checking that user is login then do next thing
+
+    const session = await getServerSession();
+    // you can get rid of the db call here 
+    
+    const user = await prismaClient.user.findFirst({
+        where:{
+            email: session?.user?.email ?? "",
+        }
+    });
+
+    if(!user){
+        NextResponse.json({
+            message:"unauthonticated "
+        },{
+            status:403
+        })
+    }
+
+
+    // after conferming user is login the voting    
+    try{
+        const data = UpvoteSchema.parse(await req.json());
+
+        await prismaClient.upvote.create({
+            data:{
+                streamId: data.streamid,
+                userId: user?.id ?? "",
+            }
+        });
+    }
+    catch(e){
+        NextResponse.json({
+            message:"error while upvoting"
+        },{
+            status: 403
+        })
+    }
+}
+
+export async function GET(req:NextRequest) {
+    const creatorId = req.nextUrl.searchParams.get("creatorId");
+    const streams = await prismaClient.stream.findMany({
+            where:{
+                userId: creatorId ?? "" 
+            }
+    })
+    return NextResponse.json({
+        streams
+    })
+}
